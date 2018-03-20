54de822f4447e4900e88e07a5271fe3777e542e1
Azon <syu.takayama@gmail.com>
2018-03-20 22:22:27 +0900

    fix: model/auth | minor fix

--- model/auth.go
+++ model/auth.go
@@ -10,7 +10,7 @@ import (
 
 //AuthType Authentication Type of each Provider
 var AuthType = map[string]string{
-	"twitter": "OAuth1",
+	"twitter": "OAuth1.0a",
 }
 var config1 = map[string]*oauth1.Config{
 	"twitter": {
@@ -24,7 +24,7 @@ var config1 = map[string]*oauth1.Config{
 //GetAuthURL Get Authorization URL
 func GetAuthURL(provider string) (*url.URL, error) {
 	switch AuthType[provider] {
-	case "OAuth1":
+	case "OAuth1.0a":
 		requestToken, _, err := config1[provider].RequestToken()
 		if err != nil {
 			return nil, fmt.Errorf("failed to get request token: %v", err)
@@ -37,7 +37,7 @@ func GetAuthURL(provider string) (*url.URL, error) {
 //Login Get Data and Generate Token
 func Login(provider string, query url.Values) (string, error) {
 	switch AuthType[provider] {
-	case "OAuth1":
+	case "OAuth1.0a":
 		accessToken, _, err := config1[provider].AccessToken(query.Get("oauth_token"), "", query.Get("oauth_verifier"))
 		if err != nil {
 			return "", fmt.Errorf("failed to get access token: %v", err)
