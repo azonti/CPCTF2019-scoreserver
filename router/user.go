e894c6717f3b2bbca23feae97b418b5218c45848
Azon <syu.takayama@gmail.com>
2018-03-24 12:39:57 +0900

    add: router.EnsureIExist

--- router/user.go
+++ router/user.go
@@ -3,6 +3,7 @@ package router
 import (
 	"git.trapti.tech/CPCTF2018/scoreserver/model"
 	"github.com/labstack/echo"
+	"net/http"
 )
 
 //DetermineMe Determine Who am I
@@ -17,6 +18,17 @@ func DetermineMe(next echo.HandlerFunc) echo.HandlerFunc {
 			return next(c)
 		}
 		c.Set("me", me)
+		return next(c)
+	}
+}
+
+//EnsureIExist Ensure I Exist
+func EnsureIExist(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		me := c.Get("me").(*model.User)
+		if me == nil {
+			return echo.NewHTTPError(http.StatusForbidden, "please log in")
+		}
 		return next(c)
 	}
 }
