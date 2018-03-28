00b5cd383d28859cb4ef5f460cce028af8fff5b1
Azon <syu.takayama@gmail.com>
2018-03-28 14:42:48 +0900

    impl: model.StartTime()

---
+++ model/etc.go
@@ -0,0 +1,12 @@
+package model
+
+import (
+	"os"
+	"time"
+)
+
+//StartTime When the Contest Start
+func StartTime() time.Time {
+	startTime, _ := time.Parse("2006-01-02 15:04:05", os.Getenv("START_TIME"))
+	return startTime
+}
