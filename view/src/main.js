add2035c9427a6fd630fbf009ef6f4bd12b32268
Azon <syu.takayama@gmail.com>
2018-04-10 18:07:59 +0900

    del: router.js

--- view/src/main.js
+++ view/src/main.js
@@ -1,6 +1,27 @@
 import Vue from 'vue'
-import router from './router'
+
+import Router from 'vue-router'
 import headful from 'vue-headful'
+
+import Index from './Index'
+import Challenges from './Challenges'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/challenges',
+      name: 'challenges',
+      component: Challenges
+    }
+  ]
+})
 
 Vue.component('vue-headful', headful)
 
