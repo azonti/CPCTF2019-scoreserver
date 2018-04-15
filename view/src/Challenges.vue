60d5fe5511220fb2a427887832a8837878a53bea
phi16 <phi16.256@gmail.com>
2018-04-15 22:49:20 +0900

    Use color

--- view/src/Challenges.vue
+++ view/src/Challenges.vue
@@ -3,21 +3,21 @@
     <vue-headful title="Challenges | CPCTF2018" />
     <div v-if="!loading">
       <div v-for="challenges in genre2Challenges">
-        <h1>{{ challenges[0].genre }}</h1>
+        <h1 :class="'chal-name-' + challenges[0].genre">{{ challenges[0].genre }}</h1>
         <div class="row">
           <div v-for="challenge in sort(challenges)" class="col-md-4">
-            <div class="panel" :class="challenge.who_solved.map(user => user.id).includes(me.id) ? 'panel-success' : 'panel-primary'">
+            <div :class="['panel', 'panel-' + challenge.genre, challenge.who_solved.map(user => user.id).includes(me.id) ? 'panel-success' : 'panel-primary']">
               <div class="panel-heading">
                 <h2 class="panel-title"><router-link :to="{name: 'challenge', params: {id: challenge.id}}">{{ challenge.name }}</router-link></h2>
               </div>
-              <div class="panel-body">
+              <div class="panel-body panel-challenge">
                 <dl class="row">
                   <dt class="col-xs-4 col-a-left">Author</dt>
                   <dd class="col-xs-8 col-a-right"><router-link :to="{name: 'user', params: {id: challenge.author.id}}"><img :src="challenge.author.icon_url" class="icon">{{ challenge.author.name }}<small v-if="challenge.author.twitter_screen_name">(@{{ challenge.author.twitter_screen_name }})</small></router-link></dd>
                 </dl>
                 <dl class="row">
                   <dt class="col-xs-4 col-a-left">Score</dt>
-                  <dd class="col-xs-8 col-a-right">{{ challenge.score }}</dd>
+                  <dd class="col-xs-8 col-a-right chal-score">{{ challenge.score }}</dd>
                 </dl>
                 <dl class="row">
                   <dt class="col-xs-4 col-a-left">Solved By</dt>
