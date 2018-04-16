dce3472a6d6a58af27c16976cfbda8344210edc5
Azon <syu.takayama@gmail.com>
2018-04-16 19:59:25 +0900

    impl: solved link

--- view/src/User.vue
+++ view/src/User.vue
@@ -23,7 +23,7 @@
               <dt class="col-xs-4">Solved</dt>
               <dd class="col-xs-8">
                 <ul class="list-unstyled">
-                  <li class="solved-chals" v-for="_solved in solved">{{ _solved.name }}</li>
+                  <li class="solved-chals" v-for="_solved in solved"><router-link :to="{ name: 'challenge', params: { id: _solved.id } }">{{ _solved.name }}</router-link></li>
                 </ul>
               </dd>
             </dl>
